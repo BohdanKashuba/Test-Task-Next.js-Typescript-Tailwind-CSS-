0d645a72fe48907446361cec526149baca876147
BohdanKashuba <bohdan.kashuba.work@gmail.com>
2024-02-13 13:26:17 +0200

    add nav menu

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -7,9 +7,9 @@ import Logo from "../../assets/images/logo.svg";
 
 const Header = () => {
   return (
-    <header className="fixed py-2 z-10 w-screen bg-gradient-to-r from-orange-200 to-green-200">
+    <header className="fixed top-0 left-0 right-0 py-2 z-10 bg-gradient-to-r from-orange-200 to-green-200">
       <Container>
-        <nav>
+        <nav className="flex items-center justify-between">
           <Link href="/">
             <Image
               src={Logo}
@@ -17,6 +17,18 @@ const Header = () => {
               className="block w-10 h-auto"
             />
           </Link>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                About
+              </Link>
+            </li>
+          </ul>
         </nav>
       </Container>
     </header>
